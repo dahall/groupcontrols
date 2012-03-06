6ae82b14ddbf654923058b122f2b56e9664acb53
dahall_cp <SND\dahall_cp>
2012-03-06 19:54:32 +0000

    * Updated version to 1.3.3 * Fixed problem with RadioButtonList problem when scanning through items for a match

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodePlex Community")]
 [assembly: AssemblyProduct("GroupControls")]
-[assembly: AssemblyCopyright("Copyright © David Hall 2011")]
+[assembly: AssemblyCopyright("Copyright © David Hall 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.2.*")]
-[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: AssemblyVersion("1.3.3.*")]
+[assembly: AssemblyFileVersion("1.3.3.0")]

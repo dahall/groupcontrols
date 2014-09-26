66f15432d1286eaf6a1b43d9f8e5b1a758da85ba
dahall_cp <SND\dahall_cp>
2014-09-26 19:51:31 +0000

    * Updated test project and added index to change event

--- TestApp/Form1.cs
+++ TestApp/Form1.cs
@@ -12,5 +12,15 @@ namespace TestApp
 				"&Peter", null, "&Quinton", null, "&Ralph", null, "&Steve", null, "&Tom", null, "&Ungar", null,
 				"&Victor", null, "&Walter", null, "&Xen", null, "&Yves", null, "&Zephyr", null);*/
 		}
+
+		private void checkBoxList1_ItemCheckStateChanged(object sender, GroupControls.CheckBoxListItemCheckStateChangedEventArgs e)
+		{
+			statusLabel.Text = string.Format("{0} item {1} checked = {2}", ((Control)sender).Name, e.ItemIndex, e.Item.Checked);
+		}
+
+		private void radioButtonList2_SelectedIndexChanged(object sender, System.EventArgs e)
+		{
+			statusLabel.Text = string.Format("{0} selectedIndex {1}", ((Control)sender).Name, radioButtonList2.SelectedIndex);
+		}
 	}
 }
